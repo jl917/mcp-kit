2e9bdf8ab2a633db9e320b72a85cc2bbf562031b
jl917 <julong1988@naver.com>
2026-07-04 22:48:55 +0900

    style(core): createMcpServer 호출을 한 줄로 포맷팅

--- packages/core/src/server.ts
+++ packages/core/src/server.ts
@@ -1,10 +1,7 @@
 import { createMcpServer, startServer } from '@common';
 import { tools } from '@/tools/index';
 
-const server = createMcpServer(
-  { name: 'mono-rele2-core', version: '1.0.0' },
-  Object.values(tools),
-);
+const server = createMcpServer({ name: 'mono-rele2-core', version: '1.0.0' }, Object.values(tools));
 
 startServer(server).catch((err) => {
   console.error('[core] server error:', err);
