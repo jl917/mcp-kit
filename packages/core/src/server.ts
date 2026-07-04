217a18e536801b6866390ab3a7614203dbd493d5
jl917 <julong1988@naver.com>
2026-07-04 23:16:39 +0900

    fix: 버저닝 테스트

--- packages/core/src/server.ts
+++ packages/core/src/server.ts
@@ -2,7 +2,7 @@ import { createMcpServer, startServer } from '@common';
 import { tools } from '@/tools/index';
 
 const server = createMcpServer({ name: 'mono-rele2-core', version: '1.0.0' }, Object.values(tools));
-
+// 서버 구동
 startServer(server).catch((err) => {
   console.error('[core] server error:', err);
   process.exit(1);
--- packages/utils/src/server.ts
+++ packages/utils/src/server.ts
@@ -1,6 +1,6 @@
 import { createMcpServer, startServer } from '@common';
 import { tools } from '@/tools';
-
+// 서버 구동
 const server = createMcpServer(
   { name: 'mono-rele2-utils', version: '1.0.0' },
   Object.values(tools),
